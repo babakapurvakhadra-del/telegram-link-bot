3275c89b63c22775ef7c7094d0bb2d09580238b2
babakapurvakhadra-del <babakapurvakhadra@gmail.com>
2026-08-28 20:32:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
@@ -11,16 +11,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot active")
 
 def main():
+    if not TOKEN:
+        raise ValueError("TELEGRAM_BOT_TOKEN not found")
+
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
     print("Bot started successfully")
 
-    # IMPORTANT: single clean polling start
-    app.run_polling(
-        drop_pending_updates=True
-    )
+    app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
     main()
