9d8aa28501cb8fb32258657df3d74f4d15a130a2
babakapurvakhadra-del <babakapurvakhadra@gmail.com>
2026-08-28 23:47:28 +0530

    Refactor app initialization for better readability

--- main.py
+++ main.py
@@ -187,7 +187,12 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE):
 
 def main():
 
-    app = ApplicationBuilder().token(TOKEN).build()
+    app = (
+    ApplicationBuilder()
+    .token(TOKEN)
+    .concurrent_updates(False)
+    .build()
+    )
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.add_error_handler(error_handler)
@@ -196,7 +201,8 @@ def main():
 
     app.run_polling(
     drop_pending_updates=True,
-    allowed_updates=Update.ALL_TYPES
+    allowed_updates=Update.ALL_TYPES,
+    stop_signals=None
     )
 
 
