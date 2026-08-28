557c3eaf6b4f459115fbe7604f6fa762db87257f
babakapurvakhadra-del <babakapurvakhadra@gmail.com>
2026-08-29 00:03:49 +0530

    Refactor app initialization for conciseness

--- main.py
+++ main.py
@@ -187,12 +187,7 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE):
 
 def main():
 
-    app = (
-    ApplicationBuilder()
-    .token(TOKEN)
-    .concurrent_updates(False)
-    .build()
-    )
+    app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.add_error_handler(error_handler)
@@ -202,7 +197,6 @@ def main():
     app.run_polling(
     drop_pending_updates=True,
     allowed_updates=Update.ALL_TYPES,
-    stop_signals=None
     )
 
 
